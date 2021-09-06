3cc5efa388514bf2fe066843d19990aeb9794ac8
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-09-06 17:39:08 +0200

    Move css files

--- src/layout/header.php
+++ src/layout/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <title>Anvil<?php if (isset($pageTitle)) echo ' - '.$pageTitle; ?></title>
   <link rel="icon" href="./favicon.ico">
-  <link rel="stylesheet" type="text/css" href="./main.min.css" />
+  <link rel="stylesheet" type="text/css" href="./css/main.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script>history.replaceState(null, null, window.location.href);</script>
 </head>
